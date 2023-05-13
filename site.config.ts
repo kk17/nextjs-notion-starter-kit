f99406ea85f6344239836a010e2465ce126a2ab5
Zhike Chen <zk.chen007@gmail.com>
2023-05-13 17:10:00 +0800

    personalize

--- site.config.ts
+++ site.config.ts
@@ -2,28 +2,30 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3c0955d204ce4d36bd1162c60acade31',
+  rootNotionPageId: 'befe03e14ae043c096ed2b09da5831fc',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
+  // rootNotionSpaceId: 'c11e0372f80d414988e1a72230e57303',
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Yeraze.com 4.0',
-  domain: 'yeraze.com',
-  author: 'Randall Hand',
+  name: 'Kyle\'s Secret Garden',
+  domain: 'kk17.net',
+  author: 'Kyle Chen',
 
   // open graph metadata (optional)
-  description: 'Yeraze.com 4.0 - Randall\'s corner of the internet',
+  description: 'Personal site of Kyle Chen aka kk17',
 
   // social usernames (optional)
-  twitter: 'yeraze',
-  github: 'yeraze',
-  linkedin: 'randallhand',
-  mastodon: 'https://indieweb.social/@Yeraze', // optional mastodon profile URL, provides link verification
+  twitter: '_kk17_',
+  github: 'kk17',
+  linkedin: 'kyle-ba7127116',
+  // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
-  privacyPolicy: '64f7bf74081041139f049af5f9b88017',
+  // privacyPolicy: '64f7bf74081041139f049af5f9b88017',
+
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
@@ -36,7 +38,7 @@ export default siteConfig({
   // whether or not redis is enabled for caching generated preview images (optional)
   // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
   // environment variables. see the readme for more info
-  isRedisEnabled: true,
+  isRedisEnabled: false,
 
   // map of notion page IDs to URL paths (optional)
   // any pages defined here will override their default URL paths
@@ -48,26 +50,19 @@ export default siteConfig({
   // }
   pageUrlOverrides: null,
 
-  googleAnalyticsId: 'G-XMZ2PCLR16',
+  googleAnalyticsId: 'G-7LKLJTQBTQ',
 
-  disqusShortname: 'yerazesdomain',
+  disqusShortname: 'kk17',
+
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  // navigationStyle: 'default'
-  navigationStyle: 'custom',
+  navigationStyle: 'default',
+  // navigationStyle: 'custom',
   navigationLinks: [
     {
-      title: 'About',
-      pageId: '21c2452a42044fb3a82da2daa46ee4d4'
-    },
-    {
-      title: 'Patents',
-      pageId: '192b09bdb8fb42399ced70bcafb6a758'
-    },
-    {
-      title: 'Publications',
-      pageId: '2547e12d13924f26a69585f5822ecab2'
+      title: 'Tech',
+      pageId: '7629c1e799304866b0417f12c765bb81'
     }
   ]
 })
